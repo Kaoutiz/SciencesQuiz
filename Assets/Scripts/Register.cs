fbbb73ed7b1ec6944129f8b08d763da05ef59954
Kaoutiz <94445745+Kaoutiz@users.noreply.github.com>
2024-02-21 18:55:11 +0100

    merge main into bryan-branch

--- Assets/Scripts/Register.cs
+++ Assets/Scripts/Register.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
 
 public class Register : MonoBehaviour
 {
     public InputField pseudoInput;
     public InputField passwordInput;
     public string apiURL = "http://localhost:3000/api/Register";
+    public Color errorColor = Color.red;
 
     [System.Serializable]
     public class UserData
@@ -17,19 +18,24 @@ public class Register : MonoBehaviour
         public string pseudo;
         public string password;
     }
-    
+
+    private void Start()
+    {
+        // Réinitialiser les champs d'entrée au démarrage
+        ResetInputFields();
+    }
+
     public void SubmitRegistration()
     {
         string pseudo = pseudoInput.text;
         string password = passwordInput.text;
 
-        // Créez une instance de UserData et définissez le nom d'utilisateur
+        // Créer une instance de UserData et définir le pseudo et le mot de passe
         UserData userData = new UserData();
         userData.pseudo = pseudo;
         userData.password = password;
 
-
-        // Convertissez l'objet UserData en une chaîne JSON à l'aide de JsonUtility
+        // Convertir l'objet UserData en une chaîne JSON
         string jsonStr = JsonUtility.ToJson(userData);
 
         StartCoroutine(SendRequest(jsonStr));
@@ -48,11 +54,45 @@ public class Register : MonoBehaviour
         if (request.result != UnityWebRequest.Result.Success)
         {
             Debug.LogError(request.error);
+            // Appliquer la couleur d'erreur aux champs d'entrée de texte
+            ChangeInputFieldColor(pseudoInput);
+            ChangeInputFieldColor(passwordInput);
+            // Réinitialiser les champs d'entrée de texte après un délai
+            StartCoroutine(ResetInputFieldsDelayed());
         }
         else
         {
             Debug.Log("Inscription réussie !");
             print(jsonStr);
+            SceneManager.LoadScene("Login");
         }
+    }
+
+    // Méthode pour appliquer la couleur d'erreur aux champs d'entrée de texte
+    void ChangeInputFieldColor(InputField inputField)
+    {
+        inputField.image.color = errorColor;
+    }
+
+    // Méthode pour réinitialiser la couleur des champs d'entrée de texte
+    void ResetInputFieldColor(InputField inputField)
+    {
+        inputField.image.color = Color.white;
+    }
+
+    // Méthode pour réinitialiser les champs d'entrée de texte après un délai
+    IEnumerator ResetInputFieldsDelayed()
+    {
+        yield return new WaitForSeconds(2f);
+        ResetInputFields();
+    }
+
+    // Méthode pour réinitialiser les champs d'entrée de texte
+    void ResetInputFields()
+    {
+        pseudoInput.text = "";
+        passwordInput.text = "";
+        ResetInputFieldColor(pseudoInput);
+        ResetInputFieldColor(passwordInput);
     }
 }
